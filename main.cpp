b71aeabcea991285078c4f3f52c24a2097f48bd8
Karl Granfors <kargra2003@skola.goteborg.se>
2022-08-29 13:08:27 +0200

    Struct test files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "returnOne.h"
+
+using namespace std;
+
+/*
+//int returnOne();
+struct Player{
+    int num;
+    Player(int n);
+};
+*/
+
+//Player::Player(int n) {this->num = n;}
+
+int main() {
+    //cout << returnOne() << endl;
+    Player p1(1);
+    cout << p1.num << endl;
+}
+
---
+++ returnOne.cpp
@@ -0,0 +1,3 @@
+#include "returnOne.h"
+Player::Player(int n) {this->num=n;}
+
