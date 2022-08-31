bbd804cfdb74e25a2eddb5eef6d5b51c88aa8850
Karl Granfors <karl@debian-BULLSEYE-live-builder-AMD64>
2022-08-31 13:46:47 +0200

    Removed comments

--- player.cpp
+++ player.cpp
@@ -6,14 +6,6 @@ using namespace std;
 int Player::do_turn(vector<int>& hand, int* your_open, int* opponents_open, int last_played) {
     
     //Display for human player
-    /*
-    cout << endl << "Player " << player_number << endl
-        << "Your hand " << hand[0] << " " << hand[1] << " " << hand[2] << endl
-        << "Your open cards  " << your_open[0] << " " << your_open[1] << " " << your_open[2] << endl
-        << "Opponents open cards  " << opponents_open[0] << " " << opponents_open[1] << " " << opponents_open[2]
-        << endl;
-    */
-    
     cout << endl << "Player " << player_number << endl
         << "Last played " << last_played << endl;
     cout << "Your hand ";
