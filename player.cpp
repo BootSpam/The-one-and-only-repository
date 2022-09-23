5d856674ac2d1128825f75a556b022da2af4c177
BootSpam <ludvig.jademyr@gmail.com>
2022-09-23 13:51:04 +0200

    Quit button

--- player.cpp
+++ player.cpp
@@ -1,6 +1,7 @@
 #include "player.h"
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 int Player::do_turn(vector<int>& real_hand, vector<int>& hand, int* your_open, int* opponents_open, int last_played) {
@@ -9,13 +10,13 @@ int Player::do_turn(vector<int>& real_hand, vector<int>& hand, int* your_open, i
     cout << endl << "Player " << player_number << endl
         << "Last played " << last_played << endl;
     
-    cout << "Your hand ";
+    cout << "Playable cards ";
     for (int i = 0; i < hand.size(); i++) {
         cout << hand[i] << " ";
     }
     cout << endl;
 
-    cout << "Complete hand ";
+    cout << "Hand ";
     for (int i = 0; i < real_hand.size(); i++) {
         cout << real_hand[i] << " ";
     }
@@ -25,10 +26,28 @@ int Player::do_turn(vector<int>& real_hand, vector<int>& hand, int* your_open, i
         << "Opponents open cards  " << opponents_open[0] << " " << opponents_open[1] << " " << opponents_open[2] << endl;
 
     //Player input
-    cout << "Play a card [0-2/3] ";
+    cout << "Play a card [0-2/3] or quit [4] ";
     cin >> this->card_to_play;
 
-    //Return card 0-2 or no card 3
-    //cout << "Return ";
-    return card_to_play;
+switch (card_to_play) {
+	case 0:
+		return card_to_play;
+		break;
+	case 1:
+		return card_to_play;
+		break;
+	case 2:
+		return card_to_play;
+		break;
+	case 3:
+		return card_to_play;
+		break;
+	case 4:
+		exit;
+		break;
+	default:
+		cout << "Nice try" << endl;
+		cout << "Play a card [0-2/3] or quit [4] ";
+		cin >> this->card_to_play;
+	}
 }
