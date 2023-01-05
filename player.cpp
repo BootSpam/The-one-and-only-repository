bff38c4311ceb2929e8132f82f4c06e5e79e58ea
BootSpam <ludvig.jademyr@gmail.com>
2023-01-05 16:13:42 +0100

    Inactivated unnecessary debug messages for speed

--- player.cpp
+++ player.cpp
@@ -14,46 +14,46 @@ Play Player::do_turn(
 	    int last_played
     ) {
     
-    cout << "Debug: do_turn succesfully called" << endl;
+    //cout << "Debug: do_turn succesfully called" << endl;
     
     Play play;
 
     //Display for human player
-    cout << "Player " << player_number << endl
-        << "Last played " << last_played << endl;
+    //cout << "Player " << player_number << endl
+        //<< "Last played " << last_played << endl;
     
-    cout << "Playable hand ";
-    for (int i = 0; i < playable_hand.size(); i++) {
-        cout << playable_hand[i] << " ";
-    }
-    cout << endl;
+    //cout << "Playable hand ";
+    //for (int i = 0; i < playable_hand.size(); i++) {
+        //cout << playable_hand[i] << " ";
+    //}
+    //cout << endl;
 
-    cout << "Real hand ";
-    for (int i = 0; i < real_hand.size(); i++) {
-        cout << real_hand[i] << " ";
-    }
-    cout << endl;
+    //cout << "Real hand ";
+    //for (int i = 0; i < real_hand.size(); i++) {
+        //cout << real_hand[i] << " ";
+    //}
+    //cout << endl;
     
-    cout << "Played cards ";
-    for (int i = 0; i < played_cards.size(); i++) {
-	    cout << played_cards[i] << " ";
-    }
-    cout << endl;
+    //cout << "Played cards ";
+    //for (int i = 0; i < played_cards.size(); i++) {
+	    //cout << played_cards[i] << " ";
+    //}
+    //cout << endl;
 
-    cout << "Your open cards "; 
-    for (int i = 0; i < your_open.size(); i++) {
-        cout << your_open[i] << " ";
-    }
-    cout << endl;
+    //cout << "Your open cards "; 
+    //for (int i = 0; i < your_open.size(); i++) {
+        //cout << your_open[i] << " ";
+   // }
+    //cout << endl;
 
-    cout << "Opponents open cards ";
-    for (int i = 0; i < opponents_open.size(); i++) {
-        cout << opponents_open[i] << " ";
-    }
-    cout << endl;
+    //cout << "Opponents open cards ";
+    //for (int i = 0; i < opponents_open.size(); i++) {
+        //cout << opponents_open[i] << " ";
+    //}
+    //cout << endl;
 
     //Player input
-    cout << "Play a card [0-" << playable_hand.size()-1 << "] " << endl;
+    //cout << "Play a card [0-" << playable_hand.size()-1 << "] " << endl;
     //cin >> this->card_to_play;
     
     /*AI*/
@@ -64,13 +64,13 @@ Play Player::do_turn(
     int highest_yet = INT_MIN;
 
     for (int i = 0; i < playable_hand.size(); i++) {
-        cout << "AI: card #" << i << " is a " << playable_hand[i] << " and is weighted " << this->mid_weights[playable_hand[i]-1] << endl;
+        //cout << "AI: card #" << i << " is a " << playable_hand[i] << " and is weighted " << this->mid_weights[playable_hand[i]-1] << endl;
         if (this->mid_weights[playable_hand[i]-1] > highest_yet) {
             this->card_to_play = i;
             highest_yet = this->mid_weights[playable_hand[i]-1];
         }
     }
-    cout << "AI: We play #" << this->card_to_play << endl;
+    //cout << "AI: We play #" << this->card_to_play << endl;
 
     /*------------------------------------------------------------*/
 
@@ -88,7 +88,7 @@ Play Player::do_turn(
 
     // Optionally play duplicates
     if (duplicates_of_chosen_card > 1) {
-        cout << "How many? [1-" << duplicates_of_chosen_card << "] " << endl;
+        //cout << "How many? [1-" << duplicates_of_chosen_card << "] " << endl;
         //cin >> this->amount_to_play;
 
         /*AI*/
@@ -107,42 +107,42 @@ Play Player::do_turn(
             amount_to_play = 1;
         }
 
-        cout << "AI: We play " << this->amount_to_play << " of the " << value_of_chosen_card << " cards" << endl;
+        //cout << "AI: We play " << this->amount_to_play << " of the " << value_of_chosen_card << " cards" << endl;
 
         /*------------------------------------------------------------*/
     }
 
     //Adjust for bad card number input
     if (playable_hand.size() == 0) {
-        cout << "Error: No playable cards" << endl;
+        //cout << "Error: No playable cards" << endl;
         return play;
     } else {
         card_to_play = card_to_play % playable_hand.size();
-        cout << "Debug: card_to_play = " << card_to_play << endl;
+        //cout << "Debug: card_to_play = " << card_to_play << endl;
     }
 
-    cout << "Debug: amount_to_play = " << amount_to_play << endl;
+    //cout << "Debug: amount_to_play = " << amount_to_play << endl;
 
     play.card_value = playable_hand[card_to_play];
     play.amount = amount_to_play;
 
-    cout << "Debug: played card value = " << play.card_value << endl;
-    cout << "Debug: played amount = " << play.amount << endl;
+    //cout << "Debug: played card value = " << play.card_value << endl;
+    //cout << "Debug: played amount = " << play.amount << endl;
 
     return play;
 }
 
 int Player::do_early_turn(vector<int>& all_cards) {
     
-    cout << "Player " << player_number << endl
-    << "Chose an open card: ";
-    for (int i = 0; i < all_cards.size(); i++) {
-        cout << all_cards[i] << " ";
-    }
-    cout << endl;
+    //cout << "Player " << player_number << endl
+    //<< "Chose an open card: ";
+    //for (int i = 0; i < all_cards.size(); i++) {
+        //cout << all_cards[i] << " ";
+    //}
+   // cout << endl;
     
     //Player input
-    cout << "Pick a card [0-" << all_cards.size()-1 << "] " << endl;
+    //cout << "Pick a card [0-" << all_cards.size()-1 << "] " << endl;
     //cin >> this->card_to_make_open;
 
     /*AI*/
@@ -153,13 +153,13 @@ int Player::do_early_turn(vector<int>& all_cards) {
     int lowest_yet = INT_MAX;
 
     for (int i = 0; i < all_cards.size(); i++) {
-        cout << "AI: card #" << i << " is a " << all_cards[i] << " and is weighted " << this->early_weights[all_cards[i]-1] << endl;
+        //cout << "AI: card #" << i << " is a " << all_cards[i] << " and is weighted " << this->early_weights[all_cards[i]-1] << endl;
         if (this->early_weights[all_cards[i]-1] < lowest_yet) {
             this->card_to_make_open = i;
             lowest_yet = this->early_weights[all_cards[i]-1];
         }
     }
-    cout << "AI: We choose #" << this->card_to_make_open << endl;
+    //cout << "AI: We choose #" << this->card_to_make_open << endl;
 
     /*------------------------------------------------------------*/
 
@@ -174,17 +174,17 @@ void Player::set_weights(int* earlies, int* mids, int* dupes) {
     }
 
     //Debug
-    cout << "Player " << this->player_number << ":" << endl;
-    for(int i = 0; i < 13; i++) {
-        cout << this->early_weights[i] << " ";
-    }
-    cout << endl;
-    for(int i = 0; i < 13; i++) {
-        cout << this->mid_weights[i] << " ";
-    }
-    cout << endl;
-    for(int i = 0; i < 13; i++) {
-        cout << this->duplicate_weights[i] << " ";
-    }
-    cout << endl;
+    //cout << "Player " << this->player_number << ":" << endl;
+    //for(int i = 0; i < 13; i++) {
+        //cout << this->early_weights[i] << " ";
+    //}
+    //cout << endl;
+    //for(int i = 0; i < 13; i++) {
+        //cout << this->mid_weights[i] << " ";
+    //}
+    //cout << endl;
+    //for(int i = 0; i < 13; i++) {
+        //cout << this->duplicate_weights[i] << " ";
+    //}
+    //cout << endl;
 }
